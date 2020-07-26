4dfa17ade777652b81180c7067b9cef577895dfb
Jorge <jbaigorria0@gmail.com>
2020-07-26 20:52:03 -0300

    CORS y Express

---
+++ database/config.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const dbConection = async() => {
+
+    try {
+        await mongoose.connect(process.env.DB_CNN, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        console.log('DB online');
+
+    } catch {
+        console.log(error);
+        throw new Error('Error al iniciar la DB ver logs');
+    }
+
+
+};
+
+module.exports = {
+    dbConection
+};
